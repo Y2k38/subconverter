13c0d2f41f6080a539eb24831605091db6f5e74c
y2k38 <1054458155@qq.com>
2026-04-05 18:42:21 +0800

    chore: bump version to 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Subconverter.MixProject do
   def project do
     [
       app: :subconverter,
-      version: "0.1.0",
+      version: "0.1.2",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
